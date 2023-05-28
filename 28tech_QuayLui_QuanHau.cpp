d4f240742860dd2e7aae0b8330b931dd6e8769f8
Võ Văn Minh <96302642+vvm1004@users.noreply.github.com>
2023-05-28 22:50:54 +0700

    Add files via upload

---
+++ 28tech_QuayLui_QuanHau.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int N, X[100], cot[100], d1[100], d2[100];
+int a[100][100], cnt = 0;
+	
+void inkq(){
+	memset(a, 0, sizeof(a));
+	for(int i = 1; i<=N; i++){
+		a[i][X[i]] = 1;
+	}
+	for(int i = 1; i<=N; i++){
+		for(int j = 1; j<=N; j++){
+			cout<<a[i][j];
+		}
+		cout<<endl;
+	}
+	cout<<endl;
+}
+void Try(int i){
+	//Duyet cac kha nang ma X[i] co the nhan duoc 
+	for(int j = 1; j<=N; j++){
+		//Chuan bi gan X[i] = j;
+		if(cot[j] == 1 && d1[i - j + N] == 1 && d2[i + j - 1] == 1){
+			X[i] = j;
+			cot[j] =  d1[i - j + N] = d2[i + j - 1]= 0;
+			if(i==N){
+				inkq();
+				++cnt;
+
+			}
+			else{
+				Try(i+1);
+			}
+			//backtrack
+			cot[j] = d1[i - j + N] =  d2[i + j - 1] = 1;
+		}
+	
+		
+	}
+}
+int main(){
+	cin >> N;
+	for(int i = 1; i<=99; i++){
+		cot[i] = d1[i] = d2[i] = 1;
+	}
+	Try(1);
+	cout<<cnt<<endl;
+	return 0;
+}
+
